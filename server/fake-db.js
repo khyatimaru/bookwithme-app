c1de2bb4ec24a813d4f66ad6bb0e5f6360bda4e3
khyatimaru <khyati.maru@gmail.com>
2019-03-07 11:24:21 +0000

    Adds booking creation functionality, adds improvements of rental routes

--- server/fake-db.js
+++ server/fake-db.js
@@ -15,39 +15,44 @@ class FakeDb {
       description: "Very nice apartment in center of the city.",
       dailyRate: 43
     },
-      {
-        title: "Modern apartment in center",
-        city: "New York",
-        street: "Time Square",
-        category: "apartment",
-        image: "https://booksync-jerga-prod.s3.amazonaws.com/uploads/rental/image/5/image.jpeg",
-        bedrooms: 1,
-        shared: false,
-        description: "Very nice apartment in center of the city.",
-        dailyRate: 11
-      },
-      {
-        title: "Old house in nature",
-        city: "Spisska Nova Ves",
-        street: "Banicka 1",
-        category: "house",
-        image: "https://booksync-jerga-prod.s3.amazonaws.com/uploads/rental/image/5/image.jpeg",
-        bedrooms: 5,
-        shared: true,
-        description: "Very nice apartment in center of the city.",
-        dailyRate: 23
-      }];
+    {
+      title: "Modern apartment in center",
+      city: "New York",
+      street: "Time Square",
+      category: "apartment",
+      image: "https://booksync-jerga-prod.s3.amazonaws.com/uploads/rental/image/5/image.jpeg",
+      bedrooms: 1,
+      shared: false,
+      description: "Very nice apartment in center of the city.",
+      dailyRate: 11
+    },
+    {
+      title: "Old house in nature",
+      city: "Spisska Nova Ves",
+      street: "Banicka 1",
+      category: "house",
+      image: "https://booksync-jerga-prod.s3.amazonaws.com/uploads/rental/image/5/image.jpeg",
+      bedrooms: 5,
+      shared: true,
+      description: "Very nice apartment in center of the city.",
+      dailyRate: 23
+    }];
 
     this.users = [{
       username: "Test User",
       email: "test@gmail.com",
       password: "testtest"
+    }, {
+      username: "Test User1",
+      email: "test1@gmail.com",
+      password: "testtest1"
     }];
   }
 
   pushDataToDb() {
 
     const user = new User(this.users[0]);
+    const user1 = new User(this.users[1]);
 
     this.rentals.forEach((rental) => {
       const newRental = new Rental(rental);
@@ -56,6 +61,7 @@ class FakeDb {
       newRental.save();
     });
     user.save();
+    user1.save();
   }
   async seedDb() {
     await this.cleanDb();
